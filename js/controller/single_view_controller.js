f4fbea780b7cb4056fd6475c1e5af69bb4ebdc3a
alexgomes09 <alex_09hg@yahoo.com>
2015-06-26 01:08:42 -0400

    Trying to add comma in-between genres

--- js/controller/single_view_controller.js
+++ js/controller/single_view_controller.js
@@ -3,5 +3,7 @@ app.controller('SingleViewController', ['$scope', 'MovieService', function ($sco
 	MovieService.getSingleView().then(function(res){
 		$scope.movie = res;	
 	})
+	var a = angular.element('.genre-list').find('li h5').contents();
+	console.log(a`);
 
 }]);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function () {
+	$('.movie ul li a').find('img').on('click',function(){
+		console.log('clicked')
+		var a = $('.genre-list li h5').val();
 
+		console.log(a);
+	})
 })
